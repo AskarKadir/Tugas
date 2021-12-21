db76f666cdea734e3f293986bf987975c5959a86
Askar Kadir <askarkadir07@gmail.com>
2021-12-21 09:20:58 +0800

    firstcommit

---
+++ src/test/java/Java/Tugas/Askar/Kadir/ApplicationTests.java
@@ -0,0 +1,13 @@
+package Java.Tugas.Askar.Kadir;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
